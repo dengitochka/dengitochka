6d3ff0cdb9eaf7f4a6b3a60d6995fbc53c20fe22
djino <malkinevgeniy437@gmail.com>
2023-01-15 02:15:25 +0300

    upd

--- src/API.js
+++ src/API.js
@@ -19,7 +19,7 @@ const corsOptions ={
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
-//app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
