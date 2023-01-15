f42282cd537f8897a39bb4afdf1274eea8040db6
djino <malkinevgeniy437@gmail.com>
2023-01-15 19:33:34 +0300

    upd

---
+++ src/global/config.js
@@ -0,0 +1,36 @@
+export default {
+  BOT_TOKEN: '986084446:AAEjtg4aTK7Jg39ZNOqJQ8njdxwwu9kz3N0', //`5738109429:AAE4cczTVS8lHo-80APdCKk-CKqsGpqxjqQ`
+  BOT_LOGIN: 'https://t.me/dengitochka_bot', //`https://t.me/roszalog_bot`,
+  DB_CONFIG: { useNewUrlParser: true, useUnifiedTopology: true },
+  DB_URL: 'mongodb+srv://root:rViAmmjHisj7M9EW@roszalog.8llppu3.mongodb.net',
+  DB_NAME: 'main',
+  WEB_APP_URL: 'https://192.168.197.1:3000',
+  API_ROOT: 'https://localhost',
+  PORT: 8080,
+  PROVIDER_TOKEN: '401643678:TEST:0508416f-dcbb-4995-a89a-cc641c5cef58'
+  // CHAT_IDS: [1303646809, 2010840813],
+  // ADMINS_IDS: [1303646809, 2010840813],
+  //CHAT_ID: -1001654902402,
+}
+
+export const COMMANDS = {
+  invest: 'invest',
+  zalog: 'zalog',
+  broker: 'broker',
+  send: 'send',
+  credit: 'credit',
+  admin: 'admin',
+  changedPrice: 'changedPrice',
+  changedPassword: 'changedPassword'
+}
+
+export const PROVIDER_TOKEN_PAYMENTS = {
+  sber: {
+    name: 'Сбер',
+    token: '401643678:TEST:580db33c-3e49-4554-baf4-7c6f7e733270'
+  },
+  payMaster: {
+    name: 'PayMaster',
+    token: '1744374395:TEST:10936d181364e26cbe8d'
+  }
+}
