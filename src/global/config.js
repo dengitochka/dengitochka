1bb64c03ad768a383c41be89d814222d9ae248c5
djino <malkinevgeniy437@gmail.com>
2023-01-17 23:44:17 +0300

    upd

--- src/global/config.js
+++ src/global/config.js
@@ -1,10 +1,10 @@
 export default {
-  BOT_TOKEN: '5738109429:AAE4cczTVS8lHo-80APdCKk-CKqsGpqxjqQ', //`5772763513:AAH3_9czBKTEBnc64C_7L6rpL_GErKPvErA`
+  BOT_TOKEN: '5772763513:AAH3_9czBKTEBnc64C_7L6rpL_GErKPvErA', //`5772763513:AAH3_9czBKTEBnc64C_7L6rpL_GErKPvErA`
   BOT_LOGIN: 'https://t.me/dengitochka_bot', //`https://t.me/roszalog_bot`,
   DB_CONFIG: { useNewUrlParser: true, useUnifiedTopology: true },
   DB_URL: 'mongodb+srv://root:rViAmmjHisj7M9EW@roszalog.8llppu3.mongodb.net',
   DB_NAME: 'main',
-  WEB_APP_URL: 'https://192.168.56.1:3000',
+  WEB_APP_URL: 'https://dengitochka-web-app.herokuapp.com', // https://dengitochka-web-app.herokuapp.com
   API_ROOT: 'https://server.dengitochka.ru',
   PORT: 8080,
   PROVIDER_TOKEN: '401643678:TEST:0508416f-dcbb-4995-a89a-cc641c5cef58'
